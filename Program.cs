a20f99ef6741ce24c4669deec2b164bf1b0ea3c6
omakargundare <sanjugundre3559@gmail.com>
2026-01-07 18:43:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SampleDotNetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a sample .NET Console Application.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# dotnet
-dotnet
+# Sample .NET Project
+
+This is a simple .NET 6 Console Application.
+
+## How to Run
+1. Install .NET 6 SDK
+2. Open terminal in project folder
+3. Run:
+   dotnet restore
+   dotnet run
